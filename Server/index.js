283b9dfacfb05325b61ac4cc37d0bbc3ba72dc48
LokoGod <it21079672@my.sliit.lk>
2023-08-13 09:07:59 +0530

    router

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
-const clientRoutes = require("./routes/client");
-const generalRoutes = require("./routes/general");
-const managementRoutes = require("./routes/management");
-const salesRoutes = require("./routes/sales");
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import clientRoutes from "./routes/client.js";
+import generalRoutes from "./routes/general.js";
+import managementRoutes from "./routes/management.js";
+import salesRoutes from "./routes/sales.js";
 
 // Configurations
 dotenv.config();
